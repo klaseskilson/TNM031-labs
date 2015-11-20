4124dde35947530423df71ae7bce26cdee49b845
Klas Eskilson <klas.eskilson@gmail.com>
2015-11-20 18:57:28 +0100

    Add settings class to common package

---
+++ lab4/src/SecureElection/Common/Settings.java
@@ -0,0 +1,10 @@
+package SecureElection.Common;
+
+/**
+ * Created by kalas on 2015-11-20.
+ */
+public class Settings {
+    public static final int CLA_PORT = 8988;
+    public static final int CTF_PORT = 8989;
+    public static final String KEYLOCATION = "./assets/";
+}
