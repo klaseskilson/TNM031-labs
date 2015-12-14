9f5cd50496eb048dff2132bd36dc1c58b333c2b8
Klas Eskilson <klas.eskilson@gmail.com>
2015-12-14 13:38:55 +0100

    Add certificates

--- lab4/src/SecureElection/CentralLegitimizationAgency.java
+++ lab4/src/SecureElection/CentralLegitimizationAgency.java
@@ -1,6 +1,8 @@
 package SecureElection;
 
 import java.util.Vector;
+
+import SecureElection.Common.Settings;
 import SecureElection.Common.Voter;
 
 /**
@@ -8,7 +10,12 @@ import SecureElection.Common.Voter;
  */
 
 public class CentralLegitimizationAgency {
-    Vector<Voter> authorizedVoters = new Vector<>();
+    // constants
+    private static final String CLATRUSTSTORE = Settings.KEYLOCATION + "CLATruststore.ks";
+    private static final String CLAKEYSTORE   = Settings.KEYLOCATION + "CLAKeystore.ks";
+    private static final String CLAPASSWORD   = "somephrase";
+
+    private Vector<Voter> authorizedVoters = new Vector<>();
 
     public static void main(String[] args) {
 
--- lab4/src/SecureElection/CentralTabulatingFacility.java
+++ lab4/src/SecureElection/CentralTabulatingFacility.java
@@ -1,10 +1,16 @@
 package SecureElection;
 
+import SecureElection.Common.Settings;
+
 /**
  * Created by Klas Eskilson on 15-11-16.
  */
 
 public class CentralTabulatingFacility {
+    // constants
+    private static final String CTFTRUSTSTORE = Settings.KEYLOCATION + "CTFTruststore.ks";
+    private static final String CTFKEYSTORE   = Settings.KEYLOCATION + "CTFKeystore.ks";
+    private static final String CTFPASSWORD   = "somephrase";
 
     public static void main(String[] args) {
 
