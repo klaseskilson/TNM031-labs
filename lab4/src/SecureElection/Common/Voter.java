dda653c6b97bcbb35612de921019e32253d4bb4e
Klas Eskilson <klas.eskilson@gmail.com>
2015-12-22 11:22:24 +0100

    Add more constructors, toString and equals to Voter

--- lab4/src/SecureElection/Common/Voter.java
+++ lab4/src/SecureElection/Common/Voter.java
@@ -1,7 +1,6 @@
 package SecureElection.Common;
 
 import java.math.BigInteger;
-import java.security.SecureRandom;
 import java.util.Random;
 
 /**
@@ -9,12 +8,24 @@ import java.util.Random;
  */
 
 public class Voter {
-    private BigInteger validationNumber;
+    private BigInteger validationNumber = BigInteger.ZERO;
     private int choice, id;
 
     public Voter() {
         Random r = new Random();
         this.id = r.nextInt(1000000000);
+        this.choice = -1;
+    }
+
+    public Voter(int choice, int id) {
+        this.choice = choice;
+        this.id = id;
+    }
+
+    public Voter(int choice) {
+        Random r = new Random();
+        this.id = r.nextInt(1000000000);
+        this.choice = choice;
     }
 
     public int getId() {
@@ -25,7 +36,39 @@ public class Voter {
         this.validationNumber = validationNumber;
     }
 
+    public BigInteger getValidationNumber() {
+        return validationNumber;
+    }
+
     public void setChoice(int choice) {
         this.choice = choice;
+    }
+
+    public String clientToCTF() {
+        return "id=" + id;
+    }
+
+    public String CTFToClient() {
+        return validationNumber.toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Voter voter = (Voter) o;
+
+        return id == voter.id;
+
+    }
+
+    @Override
+    public String toString() {
+        return "Voter{" +
+                "validationNumber=" + validationNumber +
+                ", choice=" + choice +
+                ", id=" + id +
+                '}';
     }
 }
